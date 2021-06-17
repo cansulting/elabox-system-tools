5d9f06d8488c9262fa9d3542369f0b20a255816f
Jhoemar <jhoemar_pagao@yahoo.com>
2021-06-17 22:00:47 +0800

    removed old

--- ActionCenterLib/connectorClient.go
+++
@@ -1,9 +0,0 @@
-package actioncenter
-
-type ConnectorClient interface {
-	GetState() ConnectionType
-	Open() error
-	Close()
-	Subscribe(subs SubscriptionData) (string, error)
-	Broadcast(action ActionData) error
-}
