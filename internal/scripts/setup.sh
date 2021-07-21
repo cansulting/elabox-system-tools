26ae6e2a54d01d9bbcfc5c5127802efc97fddfc1
Jhoemar <jhoemar_pagao@yahoo.com>
2021-07-21 16:49:21 +0800

    updated setup

--- internal/scripts/setup.sh
+++ internal/scripts/setup.sh
@@ -1,9 +1,18 @@
 echo "Setting development pipeline for goolang"
+echo "Optional commandline params -o(target) -a(arch))"
+cos=linux                 
+carc=arm64
 #sudo add-apt-repository ppa:longsleep/golang-backports
 #sudo apt update
 #sudo apt install golang-go
-cos=$(go env GOOS)                   
-carc=$(go env GOARCH)
+# FLAGS
+while getopts o:a flag
+do
+    case "${flag}" in
+        o) cos=${OPTARG};;
+        a) carc=${OPTARG};;
+    esac
+done
 wget https://golang.org/dl/go1.16.6.$cos-$carc.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.16.6.$cos-$carc.tar.gz
 export PATH=$PATH:/usr/local/go/bin
