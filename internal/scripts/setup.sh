f1379b04cb716de919ae22a06c230affab0c22ae
Jhoemar <jhoemar_pagao@yahoo.com>
2021-07-21 11:46:03 +0800

    updated setup script

--- internal/scripts/setup.sh
+++ internal/scripts/setup.sh
@@ -1,7 +1,13 @@
 echo "Setting development pipeline for goolang"
-sudo add-apt-repository ppa:longsleep/golang-backports
-sudo apt update
-sudo apt install golang-go
+#sudo add-apt-repository ppa:longsleep/golang-backports
+#sudo apt update
+#sudo apt install golang-go
+cos=$(go env GOOS)                   
+carc=$(go env GOARCH)
+wget https://golang.org/dl/go1.16.6.$cos-$carc.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.16.6.$cos-$carc.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+sudo source $HOME/.profile
 
 # install gcc pipelines
 sudo apt install gcc-aarch64-linux-gnu
