d37472d0c616e8e8d94dced9ffafc14c1fccaf78
Jhoemar Pagao <jhoemar_pagao@yahoo.com>
2021-08-03 12:52:49 +0000

    updated upload sh. added www to system

--- internal/scripts/upload.sh
+++ internal/scripts/upload.sh
@@ -5,15 +5,19 @@ build=1
 gspk=gs://elabox/packages/$build.box
 gspki=gs://elabox/packages/$build.json
 gsinstaller=gs://elabox/installer/$os/$arch/packageinstaller
+gsh=gs://elabox/installer/$os/$arch/installer.sh
 
 installer=../builds/$os/packageinstaller/bin/packageinstaller
 pkg=../builds/$os/system/ela.system.box
 pki=../builds/$os/system/info.json
+shi=./dlinstall.sh
 
 gsutil rm $gsinstaller
 gsutil cp $installer $gsinstaller
 gsutil cp $pkg $gspk
 gsutil cp $pki $gspki
+gsutil cp $shi $gsh
 gsutil acl ch -u AllUsers:R $gspk
 gsutil acl ch -u AllUsers:R $gspki
 gsutil acl ch -u AllUsers:R $gsinstaller
+gsutil acl ch -u AllUsers:R $gsh
