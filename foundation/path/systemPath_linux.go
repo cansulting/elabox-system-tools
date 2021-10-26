f42a2aff1d53e802a32288e9aea0a5a1896772db
Jhoemar Pagao <jhoemar_pagao@yahoo.com>
2021-10-26 12:44:57 +0000

    adde code license header

--- foundation/path/systemPath_linux.go
+++ foundation/path/systemPath_linux.go
@@ -1,10 +1,19 @@
 package path
 
-/*
-	SystemPath.go
-	Constant and variables used by the system.
-	Reference: https://help.ubuntu.com/community/LinuxFilesystemTreeOverview
-*/
+// Copyright 2021 The Elabox Authors
+// This file is part of the elabox-system-tools library.
+
+// The elabox-system-tools library is under open source LGPL license.
+// If you simply compile or link an LGPL-licensed library with your own code,
+// you can release your application under any license you want, even a proprietary license.
+// But if you modify the library or copy parts of it into your code,
+// you’ll have to release your application under similar terms as the LGPL.
+// Please check license description @ https://www.gnu.org/licenses/lgpl-3.0.txt
+
+// SystemPath.go
+// Constant and variables used by the system.
+// Reference: https://help.ubuntu.com/community/LinuxFilesystemTreeOverview
+
 
 const PATH_SYSTEM = "/usr/ela/system"              // where ela binaries will be stored
 const PATH_CACHES = "/tmp/ela"                     // dir where caches will be saved
