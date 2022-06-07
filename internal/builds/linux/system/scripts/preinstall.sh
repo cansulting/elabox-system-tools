7ff383297547a0a24fb983b5b2f82df59cc8dc42
Jhoemar Pagao <jhoemar_pagao@yahoo.com>
2022-06-07 12:46:17 +0000

    updated system install script

--- internal/builds/linux/system/scripts/preinstall.sh
+++ internal/builds/linux/system/scripts/preinstall.sh
@@ -16,54 +16,6 @@ echo '$user' | curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 echo 'Y' | sudo apt update 
 
 ############################
-## Setup user, dependent files, caching
-############################
-exists=$(grep -c "^$user:" /etc/passwd)
-if [ "$exists" == 0 ]; then
-    echo "Setting up user..."
-    echo 'exit' | sudo useradd -p $(openssl passwd -1 $passwd) -m $user
-    sudo usermod -aG sudo $user
-    sudo -s -u $user
-    echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
-
-    ############################
-    ## Firewall
-    ############################
-    echo "Setting up firewall..."
-    # open the different ports with ufw
-    sudo ufw default deny incoming
-    sudo ufw default allow outgoing
-    # SSH port
-    sudo ufw allow 22
-    # companiont app port
-    sudo ufw allow 80
-    # elabox back-end port
-    sudo ufw allow 3001
-    # elabox carrier
-    sudo ufw allow 33445
-    # Feeds
-    sudo ufw allow 10018
-    # ELA DPoS port
-    sudo ufw allow 20339
-    # ELA port for SPV peers
-    sudo ufw allow 20338
-    # ELA RPC port
-    sudo ufw allow 20336
-    # ELA REST port
-    sudo ufw allow 20334
-    # DID REST port
-    sudo ufw allow 20604
-    # DID RPC port
-    sudo ufw allow 20606
-    # DID node port
-    sudo ufw allow 20608
-    echo 'y' | sudo ufw enable
-fi
-
-# ESC NODE RPC PORT
-sudo ufw allow 20637
-
-############################
 ## Setup packages
 ############################
 isinstalled() {
@@ -101,4 +53,51 @@ if [ $(isinstalled avahi-daemon) -eq 0 ]; then
     # hostnamectl to check
     /etc/init.d/avahi-daemon restart
     systemctl restart systemd-logind.service
+fi
+
+############################
+## Setup user, dependent files, caching
+############################
+exists=$(grep -c "^$user:" /etc/passwd)
+if [ "$exists" == 0 ]; then
+    ############################
+    ## Firewall
+    ############################
+    echo "Setting up firewall..."
+    # open the different ports with ufw
+    sudo ufw default deny incoming
+    sudo ufw default allow outgoing
+    # SSH port
+    sudo ufw allow 22
+    # companiont app port
+    sudo ufw allow 80
+    # elabox back-end port
+    sudo ufw allow 3001
+    # elabox carrier
+    sudo ufw allow 33445
+    # Feeds
+    sudo ufw allow 10018
+    # ELA DPoS port
+    sudo ufw allow 20339
+    # ELA port for SPV peers
+    sudo ufw allow 20338
+    # ELA RPC port
+    sudo ufw allow 20336
+    # ELA REST port
+    sudo ufw allow 20334
+    # DID REST port
+    sudo ufw allow 20604
+    # DID RPC port
+    sudo ufw allow 20606
+    # DID node port
+    sudo ufw allow 20608
+    # ESC NODE RPC PORT
+    sudo ufw allow 20637
+    echo 'y' | sudo ufw enable
+    
+    echo "Setting up user..."
+    echo 'exit' | sudo useradd -p $(openssl passwd -1 $passwd) -m $user
+    sudo usermod -aG sudo $user
+    sudo -s -u $user
+    echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 fi
