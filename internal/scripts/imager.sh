dd31680ca8b0eb7e6b0a3571244e7b494c5ef725
Jhoemar Pagao <jhoemar_pagao@yahoo.com>
2022-06-09 13:39:18 +0000

    added imager script for create system image

---
+++ internal/scripts/imager.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# create and compress the specific usb storage
+
+shrinkp=/usr/local/bin/pishrink.sh
+output=elabox.img
+
+if [[ ! -f "$shrinkp" ]]; then
+    wget https://raw.githubusercontent.com/Drewsif/PiShrink/master/pishrink.sh
+    chmod +x pishrink.sh
+    sudo mv pishrink.sh /usr/local/bin
+fi
+
+echo "Input source storage id for image, use lsblk to view list.(eg: sdb)"
+read storage
+if [[ $storage -eq "" ]]; then
+    storage=sdb
+fi
+
+sudo dd if=/dev/$storage bs=4M conv=sparse of=_$output
+eval "$shrinkp" _$output $output
+sudo rm _$output
+echo "Image created, @$output..."
+du -sch $output
+
+
