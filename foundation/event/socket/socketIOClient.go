a476cee8c90b64c91e27bc9a150977c6cc943709
Jhoemar Pagao <jhoemar_pagao@yahoo.com>
2022-08-08 06:44:07 +0000

    remove unneccessary println

--- foundation/event/socket/socketIOClient.go
+++ foundation/event/socket/socketIOClient.go
@@ -36,7 +36,7 @@ func (s *SocketIOClient) GetState() data.ConnectionType {
 
 // implementation for connector client. let client send service requests
 func (s *SocketIOClient) SendSystemRequest(event string, action data.Action) (string, error) {
-	println("socketIOConnectorClient.SendSystemRequest", event)
+	//println("socketIOConnectorClient.SendSystemRequest", event)
 	return s.socket.Ack(event, action, time.Second*constants.TIMEOUT)
 }
 
