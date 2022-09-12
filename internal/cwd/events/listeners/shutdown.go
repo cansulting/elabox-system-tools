873203864483b3cca1065c5958c73642aeadaa5b
Bon Nicolai Mercado <bonhokage06@gmail.com>
2022-09-12 18:58:59 +0000

    added logs

--- internal/cwd/events/listeners/shutdown.go
+++ internal/cwd/events/listeners/shutdown.go
@@ -4,12 +4,14 @@ import (
 	"os"
 	"syscall"
 
+	"github.com/cansulting/elabox-system-tools/foundation/logger"
 	"github.com/cansulting/elabox-system-tools/foundation/system"
 	"github.com/ztrue/shutdown"
 )
 
 func ListenToShutdown() {
 	shutdown.AddWithParam(func(sig os.Signal) {
+		logger.GetInstance().Info().Msg("Shutdown signal received")
 		system.SetEnv("ELASHUTDOWNSTATUS", "properly_shutdown")
 	})
 	shutdown.Listen(syscall.SIGINT, syscall.SIGTERM)
--- internal/cwd/system/disk/management.go
+++ internal/cwd/system/disk/management.go
@@ -1,24 +1,25 @@
 package disk
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 
+	"github.com/cansulting/elabox-system-tools/foundation/logger"
 	"github.com/cansulting/elabox-system-tools/foundation/system"
 )
 
 
 func Check() (bool,error){
+	logger.GetInstance().Info().Msg("Checking disk")	
 	cmd := exec.Command("/bin/sh", "-c", "sudo umount -l /dev/sda; sudo fsck -a /home/elabox; sudo mount /dev/sda /home/elabox")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr	
 	err := cmd.Run()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		logger.GetInstance().Error().Err(err).Msg("Failed checking disk " + err.Error())
 		return false, err
 	}
-	fmt.Fprintln(os.Stdout)
-	system.SetEnv("ELASHUTDOWNSTATUS", "disk_checked")			
+	system.SetEnv("ELASHUTDOWNSTATUS", "disk_checked")		
+	logger.GetInstance().Info().Msg("Disk checked")	
 	return true, nil
 }
