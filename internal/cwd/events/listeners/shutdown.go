93f362095f3ee55f12c00577613b7b9b2ced93a2
Bon Nicolai Mercado <bonhokage06@gmail.com>
2022-09-12 14:28:50 +0000

    update logs

--- internal/cwd/events/listeners/shutdown.go
+++ internal/cwd/events/listeners/shutdown.go
@@ -1,7 +1,6 @@
 package listeners
 
 import (
-	"log"
 	"os"
 	"syscall"
 
@@ -10,14 +9,7 @@ import (
 )
 
 func ListenToShutdown() {
-	f, err := os.OpenFile("testlogfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
-	}
-	defer f.Close()
-	log.SetOutput(f)
 	shutdown.AddWithParam(func(sig os.Signal) {
-		log.Println("shutting down")
 		system.SetEnv("ELASHUTDOWNSTATUS", "properly_shutdown")
 	})
 	shutdown.Listen(syscall.SIGINT, syscall.SIGTERM)
