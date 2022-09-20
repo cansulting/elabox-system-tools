74c95484a6931b83a693724888e378208fec0beb
Bon Nicolai Mercado <bonhokage06@gmail.com>
2022-09-20 03:21:54 +0000

    add launch for events

--- internal/cwd/events/main.go
+++ internal/cwd/events/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "github.com/cansulting/elabox-system-tools/internal/cwd/events/listeners"
+import (
+	"github.com/cansulting/elabox-system-tools/foundation/logger"
+	"github.com/cansulting/elabox-system-tools/internal/cwd/events/listeners"
+)
 
 func main() {
+	logger.Init("events")
+	logger.GetInstance().Info().Msg("Starting events listener")
 	listeners.ListenToShutdown()
 }
