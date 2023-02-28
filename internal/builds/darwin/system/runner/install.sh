abcc3d42d4727b656890b24e4a9388967807f071
Jhoemar <jhoemar_pagao@yahoo.com>
2023-03-01 00:22:57 +0800

    updated elabox runner script

--- internal/builds/darwin/system/runner/install.sh
+++ internal/builds/darwin/system/runner/install.sh
@@ -1,6 +1,5 @@
 root=/usr/local/elabox
 sudo ./packageinstaller ./ela.system.box -s
-sudo chmod -R 0777 $root
 sudo ln -s $root/ela.system/ela.system /usr/local/bin/elabox
 echo "******Elabox Installed!*****"
-echo "type elabox to run"
+echo "type sudo elabox to run"
